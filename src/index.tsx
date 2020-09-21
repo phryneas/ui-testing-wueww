17e26e2060e341d15cb30d6e77d2b8fddd335e33
Matt Sutkowski <msutkowski@gmail.com>
2020-09-21 16:06:46 -0600

    Add notes about deferring app mounting

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 import { createClient, Provider } from "urql";
 
 import { startMock } from "./mockWorker";
-
-const params = new URLSearchParams(window.location.search);
-if (params.has("mock")) {
-  const mock = params.get("mock");
-  startMock(mock as any);
-}
 
 const client = createClient({
   url: "http://localhost:5000/graphql",
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider value={client}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const renderApp = () =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider value={client}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const params = new URLSearchParams(window.location.search);
+
+if (params.has("mock")) {
+  const mock = params.get("mock");
+
+  // Technically speaking, you should not have to do this, but it doesn't hurt.
+  // msw will try to intercept fetch before the service worker is activated and then replay requests,
+  // but we've found that in some environments this isn't guaranteed (another rabbit hole to go down :tada:). 
+  // So, this is a nice way to ensure that the worker registration promise is resolved 
+  // regardless of environment oddities (local vs CI vs teammate local etc)
+  startMock(mock as any).then(() => renderApp());
+} else {
+  renderApp();
+}
