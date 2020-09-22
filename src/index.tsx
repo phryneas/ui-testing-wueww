67e7fe955b97253d4b5e52b5eb8c70bcd4f1fdb5
Lenz Weber <lenz.weber@mayflower.de>
2020-09-22 09:59:35 +0200

    remove CRA serviceWorker, delay rendering until mock is registered

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 import { createClient, Provider } from "urql";
 
 import { startMock } from "./mockWorker";
 
-const params = new URLSearchParams(window.location.search);
-if (params.has("mock")) {
-  const mock = params.get("mock");
-  startMock(mock as any);
+if (process.env.NODE_ENV === "development") {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("mock")) {
+    const mock = params.get("mock");
+    startMock(mock as any).then(renderApp);
+  } else {
+    renderApp();
+  }
+} else {
+  renderApp();
 }
 
-const client = createClient({
-  url: "http://localhost:5000/graphql",
-});
+function renderApp() {
+  const client = createClient({
+    url: "http://localhost:5000/graphql",
+  });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider value={client}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider value={client}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
